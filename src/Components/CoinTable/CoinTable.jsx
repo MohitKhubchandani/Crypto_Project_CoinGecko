6338d110a3d7166d9a4547cf567b0286a92d8f66
Mohit04 <mohitkhubchandani88@gmail.com>
2024-08-31 02:32:20 +0530

    Added react query config

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,7 +1,31 @@
+import {useState, useEffect} from 'react';
+import { fetchCointData } from '../Navbar/fetchCoinData';
+import { useQuery } from 'react-query';
+
 function CoinTable() {
+
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error} = useQuery(['coin', page], () => fetchCointData(page, 'usd'),{
+    retry: 2,
+    retryDelay: 1000,
+    cacheTime: 1000 * 60 * 2,
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data])
+
+  if(isLoading){
+    return <div>Loading...</div>
+  };
+  if(isError){
+    return <div>Error: {error.message}</div>
+  };
+
+
   return (  
     <>
-    <div>CoinTable</div>
+    <div>CoinTable <button onClick={() => setPage(page + 1)}>Click</button> {page}</div>
     </>
   );
 };
--- src/Components/Navbar/fetchCoinData.js
+++ src/Components/Navbar/fetchCoinData.js
@@ -1,12 +1,13 @@
 import axiosInstance from '../../helpers/axiosInstances';
 
 
-export async function fetchCointData() {
+export async function fetchCointData(page = 1, currency = 'usd') {
+  const perPage = 10
     try {
-      const response = await axiosInstance.get('./coins/markets?vs_currency=usd');
+      const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
+
       console.log(response);
       return response;
-      
     } catch (error) {
       console.error(error);
       return null; 
